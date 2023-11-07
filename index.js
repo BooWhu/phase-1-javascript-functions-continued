1139d98153cd7763dc3ecada7745ddfb0b5fa70b
Eli <emmuzio0510@icloud.com>
2023-11-06 18:38:34 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  function mondayWork(activity = "to the office") {
+    return `This Monday, I will go ${activity}.`;
+  }
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`; // Use "I will" before the activity
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer"));
